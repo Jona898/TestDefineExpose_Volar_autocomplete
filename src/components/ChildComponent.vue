bc178a37bab8ef80252b0a32f5a1f410a7e720bd
Jona898 <jonathan.schaeberle@outlook.de>
2023-04-19 12:16:22 +0200

    added defineExpose with Parent and child

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const sum = ref(0);
+
+const testFunctionAdd = (val1: number, val2: number) => {
+  sum.value = val1 + val2;
+};
+
+defineExpose({ testFunctionAdd, sum });
+</script>
+
+<template>
+  <div>
+    <h2>Child Component:</h2>
+    <p>Sum: {{ sum }}</p>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import ChildComponent from "./ChildComponent.vue";
+
+const numb1 = ref(0);
+const numb2 = ref(0);
+
+const childRef = ref<typeof ChildComponent | null>(null);
+</script>
+
+<template>
+  <div>
+    <h2>ParentComponent</h2>
+    <div>
+      <label for="numb1">Number1</label>
+      <input type="number" name="numb1" id="numb1" v-model="numb1" />
+    </div>
+    <div>
+      <label for="numb2">Number2</label>
+      <input type="number" name="numb2" id="numb2" v-model="numb2" />
+    </div>
+    <div>
+      <button @click="() => childRef!.testFunctionAdd(numb1, numb2)">
+        Activate child Function
+      </button>
+    </div>
+    <p>Child Sum: {{ childRef!.sum }}</p>
+  </div>
+
+  <br />
+
+  <ChildComponent ref="childRef"></ChildComponent>
+</template>
+
+<style scoped></style>
